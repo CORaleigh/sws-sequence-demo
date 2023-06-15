eaa57e22599279b246fd32316a7b48b393c3a250
justingreco <justin.greco@gmail.com>
2023-06-15 14:08:37 -0400

    play on route change

--- map.js
+++ map.js
@@ -6,6 +6,7 @@ import OAuthInfo from "@arcgis/core/identity/OAuthInfo.js";
 import esriId from "@arcgis/core/identity/IdentityManager.js";
 
 let highlights;
+
 export const setupMap = (element) => {
   const infoCode = new OAuthInfo({
     appId: "fvYgDp4eRMEJ6KEG",
@@ -42,8 +43,8 @@ const viewLoaded = async (view) => {
   const max = (await getMaxSequence(layer, routePicker)) / 10;
   const sequencePicker = getSequencePicker(max);
   view.ui.add(sequencePicker, "top-right");
-  routePicker.watch("values", (values) => routeChanged(values, view, layer, sequencePicker));
-  sequencePicker.on("animate", () => animated(view, layer, layerView,  sequencePicker.values[0], routePicker.values[0]));  
+  routePicker.watch("values", (values) => routeChanged(values, view, layer, sequencePicker, routePicker));
+  sequencePicker.on("animate", () => animated(view, layer, layerView,  sequencePicker.values[0], routePicker.values[0], max));  
 }
 
 const getRoutePicker = (routes) => {
@@ -71,64 +72,73 @@ const getSequencePicker = (max) => {
       min: 0,
       max: max,
       steps: getSteps(max),
-      labels: getLabels(max),
+      labels: getLabels(max)
     },
     values: [0],
     playRate: 300,
     visibleElements: {
       nextButton: false,
       previousButton: false
-    }
+    },
+    loop: true
   });
   return sequencePicker;
 }
 
-const routeChanged = async (values, view, layer, valuePicker) => {
+const routeChanged = async (values, view, layer, sequencePicker, routePicker) => {
   if (values.length) {
-    
-     layer.definitionExpression = `GARBAGE = '${values[0]}'`;
+    layer.definitionExpression = `GARBAGE = '${values[0]}'`;
     const query = layer.createQuery();
     query.outSpatialReference = view.spatialReference;
     query.where = layer.definitionExpression;
     const max = (await getMaxSequence(layer, routePicker)) / 10;
-    valuePicker.component.max = max;
-    valuePicker.component.steps = getSteps(max);
-    valuePicker.component.labels = getLabels(max);
-    valuePicker.values = [0];
-    valuePicker.hidden = false;
+    sequencePicker.component.max = max;
+    sequencePicker.component.steps = getSteps(max);
+    sequencePicker.component.labels = getLabels(max);
+    sequencePicker.values = [0];
+    sequencePicker.container.hidden = false;
+    sequencePicker.play();
     const extent = await layer.queryExtent(query);
     highlights?.remove();
     view.goTo(extent);
   } else {
     layer.definitionExpression = 'GARBAGE is null';
     highlights?.remove();
-    valuePicker.hidden = true;
+    sequencePicker.container.hidden = true;
   }
 
 }
-const animated = async (view, layer, layerView, sequence, route) => {
+const animated = async (view, layer, layerView, sequence, route, max) => {
   const query = layer.createQuery();
   query.where = `Sequence = ${sequence}*10 and GARBAGE = '${route}'`;
   query.returnGeometry = true;
   query.outSpatialReference = view.spatialReference;
   const result = await layer.queryFeatures(query);
   highlights = layerView.highlight(result.features);
-  const feature = result.features[0];
-  feature.symbol = {
-          type: 'simple-marker',
-          size: 14,
-          color: '#e27728',
-          outline: {
-              width: 0,
-              color: '#e27728'
-          }
-      };
-  const graphics = view.map.findLayerById('graphics');
-  graphics.graphics.removeAll();
-  graphics.graphics.add(feature);
-  if (!view.extent.contains(result.features[0].geometry)) {
-    view.goTo({ target: result.features[0], zoom: view.zoom });
-  }        
+  highlights?.remove();
+
+  if (result.features.length) {
+    const feature = result.features[0];
+    feature.symbol = {
+            type: 'simple-marker',
+            size: 14,
+            color: '#e27728',
+            outline: {
+                width: 0,
+                color: '#e27728'
+            }
+        };
+    const graphics = view.map.findLayerById('graphics');
+    graphics.graphics.removeAll();
+    graphics.graphics.add(feature);
+    if (sequence === max - 1) {
+      highlights?.remove();
+    }
+    if (!view.extent.contains(result.features[0].geometry)) {
+      view.goTo({ target: result.features[0], zoom: view.zoom });
+    }     
+  }
+   
 }
 const getSteps = (max) => {
   const steps = [];
